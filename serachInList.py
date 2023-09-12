cf55adb41732deb095335b4f59e1a9cee05d8563
DanM <dan36@ya.ru>
2023-09-12 10:24:51 +0300

    lightened code: SearchInList.py

--- serachInList.py
+++ serachInList.py
@@ -1,27 +1,22 @@
 import random
-lengthOfList = 10 #int(input('Enter a length of the list: '))
-searchedElement = random.randint(0,100) #int(input('Enter a length of the list: '))
-list_1 = list(random.randint(0,100) for i in range(lengthOfList))
+length_of_list = 10
+searched_element = random.randint(0,20)
+list_1 = list(random.randint(0,20) for i in range(length_of_list))
 
 print(list_1)
-print(searchedElement)
-found = False
-for i in list_1:
-    if i == searchedElement:
-        print(f'Searched element found on the {list_1.index(searchedElement)} position')
-        found = True
-        break
+print(searched_element)
 
-if found == False:
-    for i in range(100):
-        for j in list_1:
-            if j - i == searchedElement and found == False:
-                print(f'The closest to the searched element is {j}, found on the {list_1.index(searchedElement+i)} position')
-                found = True
-            if j + i == searchedElement and found == False:
-                print(f'The closest to the searched element is {j}, found on the {list_1.index(searchedElement-i)} position')
-                found = True
+if searched_element in list_1:
+    print(f'Found {searched_element}!')
 
+else:
+    for i in range(20):
+        if searched_element - i in list_1:
+                print(f'The closest to the searched element found is {searched_element - i}.')
+                break
+        elif searched_element + i in list_1:
+                print(f'The closest to the searched element found is {searched_element + i}.')
+                break
 
 
 
